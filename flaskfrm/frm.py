16e4b701b73902776d28eff1f73245edd93b8de2
JorgeRmz12 <jorge.rgzz04@gmail.com>
2024-09-04 23:30:53 -0600

    insert flask

---
+++ flaskfrm/frm.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from app import app
+from dbconfig import getDBConnection
+import pymysql
+
+
+@app.route('/', methods=["GET"])
+def index():
+    connection = getDBConnection()
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    try:
+        cursor.execute("SELECT * FROM personas")
+        personas = cursor.fetchall()
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
+        personas = []
+    finally:
+        cursor.close()
+        connection.close()
+
+    return render_template('frm.html', personas=personas)
+
+@app.route('/', methods=['POST'])
+def submit():
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    
+    connection = getDBConnection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("INSERT INTO personas (nombre, apellido) VALUES (%s,%s)", (nombre, apellido))
+        connection.commit()
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        connection.close()
+
+    return redirect(url_for('index'))    
+
+
+if __name__ == "__main__":
+    app.run()
